dbae1d9590205bdb7b6432f0c976d4b7ec1a0059
Daniel San <danielsan@ilhasoft.com.br>
2016-12-20 00:17:39 -0300

    Added documentation for isNew2 function
    Signed-off-by: Daniel San <danielsan@ilhasoft.com.br>

--- extensions/parse.ts
+++ extensions/parse.ts
@@ -1,16 +1,22 @@
-// Reference: http://stackoverflow.com/questions/18835190/extend-basic-types-in-typescript-error-this-is-not-defined
+/// <reference path="../typings/parse.d.ts" />
 
 declare namespace Parse {
 
     interface Object {
 
+        /**
+         * Method that should be used in the afterSave trigger (from the cloud code)
+         *   to know if the object was saved now.
+         * @returns true if this object has never been saved to Parse.
+         */
         isNew2: () => boolean;
 
     }
 
 }
 
+// Reference: http://stackoverflow.com/questions/18835190/extend-basic-types-in-typescript-error-this-is-not-defined
 Parse.Object.prototype.isNew2 = function (): boolean {
     const obj: Parse.Object = this;
-    return obj.isNew() || (obj.updatedAt.getTime() === obj.createdAt.getTime());
+    return (obj.updatedAt.getTime() === obj.createdAt.getTime());
 }
