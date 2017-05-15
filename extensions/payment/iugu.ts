971c517f209b0dc8e92cca65472ffcf462a7e990
Dielson Sales <dielson@ilhasoft.com.br>
2017-05-15 17:44:24 -0300

    Change iugu.ts functions to use Parse.Promise

--- extensions/payment/iugu.ts
+++ extensions/payment/iugu.ts
@@ -35,7 +35,7 @@ function getCustomer(customerId: string) {
  * @param {Object} data: { email, name, notes, cpfCnpj, zipCode, number, street, city, state, district, complement, customVariables }
  * @returns {Object}
  */
-function createCustomer(data: { [key: string]: any}) {
+function createCustomer(data: { [key: string]: any}): Parse.Promise<any> {
   return sendToRest({
     api_token: apiToken,
     email: data["email"],
@@ -58,7 +58,7 @@ function createCustomer(data: { [key: string]: any}) {
  * @param {String} customerId
  * @returns {Object}
  */
-function getCustomerPaymentMethods(customerId: string) {
+function getCustomerPaymentMethods(customerId: string): Parse.Promise<any> {
   return getFromRest({
     api_token: apiToken,
     customer_id: customerId,
@@ -69,7 +69,7 @@ function getCustomerPaymentMethods(customerId: string) {
  * Create credit card token
  * @param {Object} data: { number, CVV, firstName, lastName, month, year }
  */
-function createToken(data: { [key: string]: any}, method: string = 'credit_card', test: boolean = true) {
+function createToken(data: { [key: string]: any}, method: string = 'credit_card', test: boolean = true): Parse.Promise<any> {
   return sendToRest({
     account_id: accountId,
     method: method,
@@ -92,7 +92,7 @@ function createToken(data: { [key: string]: any}, method: string = 'credit_card'
  * @param {String} description
  * @param {Boolean} isDefault
  */
-function createPaymentMethod(customerId: string, tokenId: string, description: string = '', isDefault: boolean = true) {
+function createPaymentMethod(customerId: string, tokenId: string, description: string = '', isDefault: boolean = true): Parse.Promise<any> {
   return sendToRest({
     api_token: apiToken,
     customer_id: customerId,
@@ -107,7 +107,7 @@ function createPaymentMethod(customerId: string, tokenId: string, description: s
  * @param {string} customerId
  * @param {string} paymentId
  */
-function deletePaymentMethod(customerId: string, paymentMethodId: string) {
+function deletePaymentMethod(customerId: string, paymentMethodId: string): Parse.Promise<any> {
     return deleteFromRest({
       api_token: apiToken
     }, `customers/${customerId}/payment_methods/${paymentMethodId}`);
@@ -121,7 +121,7 @@ function deletePaymentMethod(customerId: string, paymentMethodId: string) {
  * @param {Array} items: { description, quantity, price_cents }
  * @returns {Object}
  */
-function createInvoice(email: string, customerId: string, dueDate: string, items: Array<{}>) {
+function createInvoice(email: string, customerId: string, dueDate: string, items: Array<{}>): Parse.Promise<any> {
   return sendToRest({
     api_token: apiToken,
     email: email,
@@ -138,7 +138,7 @@ function createInvoice(email: string, customerId: string, dueDate: string, items
  * @param {String} invoiceId
  * @returns {Object}
  */
-function createChargeCustomer(cpmId: string, customerId: string, invoiceId: string, tokenId: string) {
+function createChargeCustomer(cpmId: string, customerId: string, invoiceId: string, tokenId: string): Parse.Promise<any> {
   const data: any = {
     api_token: apiToken,
     customer_id: customerId,
@@ -164,7 +164,7 @@ function createChargeCustomer(cpmId: string, customerId: string, invoiceId: stri
  * @param {String} method
  * @returns {Object}
  */
-function createCharge(email: string, items: Array<{}>, tokenId: any = null, method: string = 'bank_slip') {
+function createCharge(email: string, items: Array<{}>, tokenId: any = null, method: string = 'bank_slip'): Parse.Promise<any> {
   const data: any = {
     api_token: apiToken,
     email: email,
@@ -187,121 +187,101 @@ function createCharge(email: string, items: Array<{}>, tokenId: any = null, meth
  * @param {Object} data 
  * @returns {Object}
  */
-function createNewPayment(data: any) {
+function createNewPayment(data: any): Parse.Promise<any> {
   const iugu = initIugu(data.tokenId, data.accountId);
-  return new Promise((resolve: any, reject: any) => {
-    let dataReturn = {};
-    if (data.directPayment === true) {
-      if (data.creditCard) {
-        return createToken(data.creditCard).then((token: any) => {
-          return token;
-        }).then((token) => {
-          return createCharge(data.customer.email, data.items, token.id);
-        }).then((response: any) => {
-          resolve(response);
-        }).catch((error: any) => {
-          reject(error);
-        });
-      } else {
-        reject('CREDIT CARD REQUIRED');
-      }
+  let promise = new Parse.Promise();
+  let dataReturn = {};
+  if (data.directPayment === true) {
+    if (data.creditCard) {
+      createToken(data.creditCard).then((token: any) => {
+        return createCharge(data.customer.email, data.items, token.id);
+      }).then((response: any) => {
+        promise.resolve(response);
+      }, (error) => {
+        promise.reject(error);
+      });
     } else {
-      if (data.customer.id) {
-        return getCustomer(data.customer.id).then((customer: any) => {
-          return customer;
-        }).then((customer) => {
-          if (data.creditCard.id === undefined) {
-            return paymentWithNewCreditCard(customer, data);
-          } else {
-            return paymentWithPaymentMethod(customer, data);
-          }
-        }).then((response: any) => {
-          resolve(response);
-        }).catch((error: any) => {
-          reject(error);
-        });
-      } else {
-        return createCustomer(data.customer).then((customer: any) => {
-          return customer;
-        }).then((customer) => {
-          return paymentWithNewCreditCard(customer, data);
-        }).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        });
-      }
+      return Parse.Promise.error('CREDIT CARD REQUIRED');
     }
-  });
+  } else {
+    if (data.customer.id) {
+      getCustomer(data.customer.id).then((customer: any) => {
+        if (data.creditCard.id === undefined) {
+          return paymentWithNewCreditCard(customer, data);
+        } else {
+          return paymentWithPaymentMethod(customer, data);
+        }
+      }).then((response: any) => {
+        promise.resolve(response);
+      }, (error) => {
+        promise.reject(error);
+      });
+    } else {
+      createCustomer(data.customer).then((customer: any) => {
+        return paymentWithNewCreditCard(customer, data);
+      }).then((response) => {
+        promise.resolve(response);
+      }, (error) => {
+        promise.reject(error);
+      });
+    }
+  }
+  return promise;
 };
 
-function paymentWithNewCreditCard(customer: any, data: any) {
-  return new Promise((resolve: any, reject: any) => {
-    if (data.creditCard) {
-      const dataReturn: any = {};
-      dataReturn.customer = customer;
-      
-      return createToken(data.creditCard).then((token: any) => {
-        dataReturn.token = token;
-        return token;
-      }).then((token) => {
-        if (customer && token && data.creditCard.save === true) {
-          return createPaymentMethod(customer.id, token.id, data.creditCard.description, data.creditCard.isDefault).then((cpm: any) => {
-            dataReturn.cpm = cpm;
-            return cpm;
-          }).then((cpm) => {
-            return createInvoice(customer.email, customer.id, data.dueDate, data.items);
-          }).then((invoice) => {
-            return createChargeCustomer(dataReturn.cpm["id"], customer["id"], invoice["id"], '');
-          }).then((charge: any) => {
-            dataReturn.charge = charge;
-            resolve(dataReturn);
-          }).catch((error: any) => {
-            reject(error);
-          });
-        } else {
-          return createInvoice(customer.email, customer.id, data.dueDate, data.items).then((invoice: any) => {
-            dataReturn.invoice = invoice;
-            return invoice;
-          }).then((invoice) => {
-            return createChargeCustomer('', customer.id, invoice.id, token.id).then((charge: any) => {
-              dataReturn.charge = charge;
-              return charge;
-            });
-          }).then(() => {
-            resolve(dataReturn);
-          }).catch((error: any) => {
-            reject(error);
-          });
-        }
-      });
-    } else {
-      reject('CREDIT CARD DATA REQUIRED');
-    }
-  });
-}
-
-function paymentWithPaymentMethod(customer: any, data: any) {
-  return new Promise((resolve: any, reject: any) => {
-    if (data.creditCard.id) {
-      const dataReturn: any = {};
-      dataReturn.customer = customer;
-
-      return createInvoice(customer.email, customer.id, data.dueDate, data.items).then((invoice: any) => {
-        dataReturn.invoice = invoice;
-        return invoice;
+function paymentWithNewCreditCard(customer: any, data: any): Parse.Promise<any> {
+  if (!data.creditCard) {
+    return Parse.Promise.error("CREDIT CARD DATA REQUIRED");
+  }
+  let promise = new Parse.Promise();
+  const dataReturn: any = {};
+  dataReturn.customer = customer;
+  createToken(data.creditCard).then((token: any) => {
+    dataReturn.token = token;
+    if (customer && token && data.creditCard.save === true) {
+      createPaymentMethod(customer.id, token.id, data.creditCard.description, data.creditCard.isDefault).then((cpm: any) => {
+        dataReturn.cpm = cpm;
+        return createInvoice(customer.email, customer.id, data.dueDate, data.items);
       }).then((invoice) => {
-        return createChargeCustomer(data.creditCard.id, customer.id, invoice.id, '');
+        return createChargeCustomer(dataReturn.cpm["id"], customer["id"], invoice["id"], '');
       }).then((charge: any) => {
         dataReturn.charge = charge;
-        resolve(dataReturn);
-      }).catch((error) => {
-        reject(error);
+        promise.resolve(dataReturn);
+      }, (error) => {
+        promise.reject(error);
       });
     } else {
-      reject('PAYMENT METHOD ID REQUIRED');
+      createInvoice(customer.email, customer.id, data.dueDate, data.items).then((invoice: any) => {
+        dataReturn.invoice = invoice;
+        return createChargeCustomer('', customer.id, invoice.id, token.id);
+      }).then((charge: any) => {
+        dataReturn.charge = charge;
+        promise.resolve(dataReturn);
+      }, (error) => {
+        promise.reject(error);
+      });
     }
   });
+  return promise;
+}
+
+function paymentWithPaymentMethod(customer: any, data: any): Parse.Promise<any> {
+  if (!data.creditCard.id) {
+    return Parse.Promise.error('PAYMENT METHOD ID REQUIRED');
+  }
+  let promise = new Parse.Promise<any>();
+  const dataReturn: any = {};
+  dataReturn.customer = customer;
+  createInvoice(customer.email, customer.id, data.dueDate, data.items).then((invoice: any) => {
+    dataReturn.invoice = invoice;
+    return createChargeCustomer(data.creditCard.id, customer.id, invoice.id, '');
+  }).then((charge: any) => {
+    dataReturn.charge = charge;
+    promise.resolve(dataReturn);
+  }, (error) => {
+    promise.reject(error);
+  });
+  return promise;
 }
 
 /**
@@ -309,7 +289,7 @@ function paymentWithPaymentMethod(customer: any, data: any) {
  * @param {String} apiToken
  * @param {String} invoiceId 
  */
-function refundInvoice(apiToken: String, invoiceId: String) {
+function refundInvoice(apiToken: String, invoiceId: String): Parse.Promise<any> {
   return sendToRest({
     api_token: apiToken,
     id: invoiceId,
@@ -321,7 +301,7 @@ function refundInvoice(apiToken: String, invoiceId: String) {
  * @param {String} apiToken 
  * @param {String} invoiceId 
  */
-function cancelInvoice(apiToken: string, invoiceId: string) {
+function cancelInvoice(apiToken: string, invoiceId: string): Parse.Promise<any> {
   return putToRest({
     api_token: apiToken,
     id: invoiceId,
@@ -335,16 +315,15 @@ function sendToRest(data: Object, restPoint: string) {
     json: true,
     body: data,
   };
-
-  return new Promise((resolve: any, reject: any) => {
-    request.post(options, (err: any, httpResponse: any, body: any) => {
-      if (err) {
-        reject(err);
-      } else if (body) {
-        resolve(body);
-      }
-    });
+  let promise = new Parse.Promise();
+  request.post(options, (err: any, httpResponse: any, body: any) => {
+    if (err) {
+      promise.reject(err);
+    } else if (body) {
+      promise.resolve(err);
+    }
   });
+  return promise;
 }
 
 function putToRest(data: Object, restPoint: string) {
@@ -354,52 +333,49 @@ function putToRest(data: Object, restPoint: string) {
     json: true,
     body: data,
   };
-
-  return new Promise((resolve, reject) => {
-    request.put(options, (err: any, httpResponse: any, body: any) => {
-      if (err) {
-        reject(err);
-      } else if (body) {
-        resolve(body);
-      }
-    });
+  let promise = new Parse.Promise();
+  request.put(options, (err: any, httpResponse: any, body: any) => {
+    if (err) {
+      promise.reject(err);
+    } else if (body) {
+      promise.resolve(body);
+    }
   });
+  return promise;
 }
 
-function getFromRest(data: Object, restPoint: string) {
+function getFromRest(data: Object, restPoint: string): Parse.Promise<any> {
   const options = {
     url: `${urlApi}${restPoint}`,
     contentType: 'application/json',
     json: true,
     body: data,
   };
-
-  return new Promise((resolve: any, reject: any) => {
-    request.get(options, (err: any, httpResponse: any, body: any) => {
-      if (err) {
-        reject(err);
-      } else if (body) {
-        resolve(body);
-      }
-    });
+  let promise = new Parse.Promise();
+  request.get(options, (err: any, httpResponse: any, body: any) => {
+    if (err) {
+      promise.reject(err);
+    } else if (body) {
+      promise.resolve(body);
+    }
   });
+  return promise;
 }
 
-function deleteFromRest(data: Object, restPoint: string) {
+function deleteFromRest(data: Object, restPoint: string): Parse.Promise<any> {
   const options = {
     url: `${urlApi}${restPoint}`,
     contentType: 'application/json',
     json: true,
     body: data,
   };
-
-  return new Promise((resolve: any, reject: any) => {
-    request.del(options, (err: any, httpResponse: any, body: any) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(body);
-      }
-    });
+  let promise = new Parse.Promise();
+  request.del(options, (err: any, httpResponse: any, body: any) => {
+    if (err) {
+      promise.reject(err)
+    } else {
+      promise.resolve(body);
+    }
   });
+  return promise;
 }
